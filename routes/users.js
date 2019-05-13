6eede23931f659e55a2f240378f5411e1e64d38a
sureshak07 <suresh.ak@iinerds.com>
2019-05-13 23:56:14 +0530

    config added for mysql

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http')
+var mysql = require('../config');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,8 +13,24 @@ router.get('/', function(req, res, next) {
        "id": 5
     }
  }
- 
   res.send(user);
 });
+
+router.post('/' , function(req,res){
+  var name = req.body.name;
+  var sex = req.body.sex;
+  var age = req.body.age;
+  var slot= req.body.slot;
+  var phone = req.body.phone;
+  var description = req.body.description;
+console.log("name : "+name+" sex : "+sex+" age : "+age+" phone : "+phone+" description : "+description);
+con.connect();
+var post  = {name: name, sex: sex, age: age, phone: phone, description: description ,time_slot: slot};
+con.query('INSERT INTO appointment VALUES ?', post, function(err, result) {
+  if (err) throw err;
+     console.log(result);
+     res.send(result);
+  }); 
+})
 
 module.exports = router;
